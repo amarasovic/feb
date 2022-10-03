075e2f33861553ad0e6324357f73ee35141825d5
Ana Marasovic <anam@allenai.org>
2022-10-03 14:09:49 -0600

    use chpc

--- scripts/exp.py
+++ scripts/exp.py
@@ -79,7 +79,7 @@ def collect_results(args):
 
 
 # ===========> Code for running models with 60 seeds; eval on dev sets will be done jointly with training and results recorded in logger.log that we will use to collect results 
-seeds_fewshot = [7004, 3639, 6290, 9428, 7056, 4864, 4273, 7632, 2689, 8219, 4523, 2175, 7356, 8975, 51, 4199, 4182, 1331, 2796, 6341, 7009, 1111, 1967, 1319, 741, 7740, 1335, 9933, 6339, 3112, 1349, 8483, 2348, 834, 6895, 4823, 2913, 9962, 178, 2147, 8160, 1936, 9991, 6924, 6595, 5358, 2638, 6227, 8384, 2769, 4512, 2051, 4779, 2498, 176, 9599, 1181, 5320, 588, 4791]
+seeds_fewshot = [7004] #, 3639, 6290, 9428, 7056, 4864, 4273, 7632, 2689, 8219, 4523, 2175, 7356, 8975, 51, 4199, 4182, 1331, 2796, 6341, 7009, 1111, 1967, 1319, 741, 7740, 1335, 9933, 6339, 3112, 1349, 8483, 2348, 834, 6895, 4823, 2913, 9962, 178, 2147, 8160, 1936, 9991, 6924, 6595, 5358, 2638, 6227, 8384, 2769, 4512, 2051, 4779, 2498, 176, 9599, 1181, 5320, 588, 4791]
 
 experiments = {}
 # You can add your own values as a new key and run those experiments with `---experiment_id <your_experiment_key>`
@@ -264,7 +264,7 @@ def run_exp(args):
                 cmd_prefix = "PYTHONPATH=. deepspeed input_to_label_and_rationale.py "
                 cmd_batch_size = f" --per_device_train_batch_size 1 --per_device_eval_batch_size 1 --gradient_accumulation_steps {per_device_train_batch_size} "
             else:
-                cmd_prefix = "PYTHONPATH=. python input_to_label_and_rationale.py "
+                cmd_prefix = "PYTHONPATH=. salloc -n 1 -N 1 -t 1:00:00 -p marasovic-gpu-np -A  marasovic-gpu-np  --gres=gpu:a100:1 --mem=12G 50G python input_to_label_and_rationale.py "
                 cmd_batch_size = f" --per_device_train_batch_size {per_device_train_batch_size} --per_device_eval_batch_size 64 --gradient_accumulation_steps 1 "
 
             cmd = f'''{cmd_prefix} \
